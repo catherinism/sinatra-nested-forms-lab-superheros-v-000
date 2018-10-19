5005e0c475831941da553b5ae208689a10b5bfab
Catherine Salazar <catcreates13@gmail.com>
2018-10-19 02:08:57 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
 
       @team = Team.new(params[:team])
 
-      params[:team][:superhero].each do |details|
+      params[:team][:members].each do |details|
         Superhero.new(details)
       end
 
