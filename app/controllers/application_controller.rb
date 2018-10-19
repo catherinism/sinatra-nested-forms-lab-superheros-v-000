b88fb789162287408c15d3cecb98a54eef89c3cc
Catherine Salazar <catcreates13@gmail.com>
2018-10-19 00:28:47 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,12 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
-  binding.pry
+      
       erb :super_hero
     end
 
     post '/teams' do
-      binding.pry
+
       #  erb :super_hero
     end
 
